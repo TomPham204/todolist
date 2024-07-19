ad916490a19e8d16a8da3c3820f7bef77344806c
TomPham204 <thect04@gmail.com>
2024-07-19 15:23:59 +0700

    feat: get by id endpoint for /user

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import { UserDto } from "@/dto/user.dto";
 import { UserService } from "@/services/user.service";
 
 export class UserController {
@@ -5,8 +6,11 @@ export class UserController {
 
 	async getUsers() {}
 
-	async getUserById(id: string) {}
-	
+	async getUserById(id: string): Promise<UserDto> {
+		const user = await this.userService.getUserById(parseInt(id));
+		return <UserDto>user;
+	}
+
 	async updateUserById(id: string) {}
 
 	async deleteUserById(id: string) {}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -11,7 +11,17 @@ userRouter.get("/", async (req, res) => {
 
 userRouter.get("/:id", async (req, res) => {
 	try {
-	} catch (error) {}
+		const id = req.params.id;
+		if (id != null && !Number.isInteger(Number(id)))
+			return res.status(400).json({ message: "Invalid user id" });
+
+		const user = await userController.getUserById(id);
+		if (!user) return res.status(404).json({ message: "User not found" });
+
+		return res.status(200).send(user);
+	} catch (error) {
+		return res.status(500).json({ message: (error as Error).message });
+	}
 });
 
 userRouter.put("/:id", async (req, res) => {
